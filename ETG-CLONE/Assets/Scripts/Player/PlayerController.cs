dcee69a87635c463a4b40317c82e17e5df1c9ef5
puleng_manganye <1915865@students.wits.ac.za>
2023-08-17 15:16:08 +0200

    2nd commit
    - Added Tile-based movement
    - Added Player animations
    - Added collisions with solid objects

--- ETG-CLONE/Assets/Scripts/Player/PlayerController.cs
+++ ETG-CLONE/Assets/Scripts/Player/PlayerController.cs
@@ -1,57 +1,147 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
+
+#region Class Description:
+/*
+ *  This script controls the Player's tile-based movement.
+ */
+#endregion
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    Rigidbody2D rb;
+    #region Fields
 
-    Vector2 moveDir;
+    // Movement speed
+    public float moveSpeed;
+
+    // Solid objects layer
+    public LayerMask solidObjectsLayer;
+
+    // Level Geo layer
+    public LayerMask levelGeoLayer;
+
+    // Is Player moving?
+    private bool _isMoving;
+
+    // Input
+    private Vector2 _input;
+
+    // Animator controller
+    private Animator _animator;
+
+    // Player
+    private GameObject _player;
+
+    #endregion
+
+    #region Intialisations
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        // Get References
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _animator = _player.GetComponentInChildren<Animator>();
     }
+
+    #endregion
+
+    #region Move the Player
 
     private void Update()
     {
-        ProcessInputs();
-    }
+        // If Player is not moving
+        if (!_isMoving)
+        {
+            // Check for _input to move the Player
+            // Left arrow = -1, Right arrow = +1
+            _input.x = Input.GetAxisRaw("Horizontal");
+            // Down arrow = -1, Up arrow = +1
+            _input.y = Input.GetAxisRaw("Vertical");
 
-    private void FixedUpdate()
+            // If _input is not 0
+            if (_input != Vector2.zero)
+            {
+                // Set the moveX parameter for Idle
+                _animator.SetFloat("moveX", _input.x);
+
+                // Set the moveY parameter for Idle
+                _animator.SetFloat("moveY", _input.y);
+
+
+                // Calculate the target position to which the Player should move to
+                // The target postion = the current position of the Player + the _input
+                var targetPos = transform.position;
+                targetPos.x += _input.x;
+                targetPos.y += _input.y;
+
+                // If tile is walkable
+                if (isWalkable(targetPos))
+                {
+                    // Move the Player from her current position to his target position
+                    StartCoroutine(Move(targetPos));
+                }
+            }
+        }
+
+        // Set the _isMoving parameter for Walk
+        _animator.SetBool("isMoving", _isMoving);
+    }
+    #endregion
+
+    #region Move
+
+    IEnumerator Move(Vector3 targetPos)
     {
-        Move();
-        HandleRotation();
+        // Move the Player from her current position to her target position
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            // The Player is moving
+            _isMoving = true;
+
+            // If there is a difference which is greater than a small value between the two positions,
+            // We use the MoveTowards() function to move the Player towards his target position by a very small amount
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+            // This will stop the execution of the Coroutine and resume it in the next Update() function
+            yield return null;
+        }
+
+        // Set the current position of the Player to the target position
+        transform.position = targetPos;
+
+        // The Player is not moving
+        _isMoving = false;
+
+        // Check for encounters with NPCs on the Level Geo tiles
+        CheckForEncounters();
     }
 
-
-    void ProcessInputs()
+    private bool isWalkable(Vector3 targetPos)
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        // Will take the target position and check if the tile at that position is walkable
+        // Check if there's a solid object at a position
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        {
+            // Tile is not walkable
+            return false;
+        }
 
-        moveDir = new Vector2 (moveX, moveY).normalized;
+        // Tile is walkable
+        return true;
     }
+    #endregion
 
-    void Move()
+    #region Check For Encounters
+
+    private void CheckForEncounters()
     {
-        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        // Check if the tile the Player is walking on is a Level Geo tile
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, levelGeoLayer) != null)
+        {
+            // You encountered an NPC
+            // Debug.Log("You encountered an NPC.");
+        }
     }
-
-    //Aim
-    [SerializeField] Transform weaponHolder;
-    [SerializeField] float offset;
-
-    void HandleRotation()
-    {
-        Vector3 displacement = weaponHolder.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Mathf.Atan2(displacement.y, displacement.x) * Mathf.Rad2Deg;
-        weaponHolder.rotation = Quaternion.Euler(0, 0, angle -180);
-
-       
-    }
-
-
+    #endregion
 }
