ef1200c41b01316bbe1df8e6c6d044387af67faf
puleng_manganye <1915865@students.wits.ac.za>
2023-08-17 16:32:13 +0200

    3rd commit

---
+++ ETG-CLONE/Assets/Scripts/Settings/MenuSettings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+#region Class Description:
+/*
+ * 
+ */
+#endregion
+
+public class MenuSettings : MonoBehaviour
+{
+    #region Fields
+
+    // Audio mixer component
+    public AudioMixer audioMixer;
+    
+    #endregion
+
+    #region Set volume
+    
+    //This function is called when we adjust the volume slider.
+    public void SetMusicVolume (float volume)
+    {
+        //Debug.Log(volume);
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetAmbienceVolume (float volume)
+    {
+        //Debug.Log(volume);
+        audioMixer.SetFloat("SpatialVolume", volume);
+    }
+    
+    public void SetSfxVolume (float volume)
+    {
+        //Debug.Log(volume);
+        audioMixer.SetFloat("SfxVolume", volume);
+    }
+    #endregion
+}
---
+++ ETG-CLONE/Assets/Scripts/UI/AutoUISceneLoader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region Class Description:
+/*
+ *  This script automatically loads a scene.
+ */
+#endregion
+
+public class AutoUISceneLoader : MonoBehaviour
+{
+    #region Fields
+
+    public string sceneName;
+    public bool autoStart;
+    public float waitTime;
+    private UISceneLoader _sceneLoader;
+
+    #endregion
+
+    #region Load scene
+    private void Start()
+    {
+        // Get references.
+        _sceneLoader = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<UISceneLoader>();
+
+        if (autoStart)
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        // Load completePanel screen
+        _sceneLoader.LoadScene(sceneName);
+    }
+    #endregion
+}
